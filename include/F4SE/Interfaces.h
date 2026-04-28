29a7fa6a1f6bc9194a3db121ca6c7bf0f74e3a9c
Alexey <vlad.shabalin.1992@mail.ru>
2026-04-28 16:57:21 +0300

    typo

--- include/F4SE/Interfaces.h
+++ include/F4SE/Interfaces.h
@@ -504,7 +504,7 @@ namespace F4SE
 		constexpr void HasNoStructUse(const bool a_value) noexcept { SetOrClearBit(structureIndependence, 1 << 0, a_value); }
 	
 		constexpr void IsLayoutDependent(const bool a_value) noexcept { IsLayoutDependentAE(a_value); }
-		// 1 << 2 is for runtime 1.10.980 and 1.10.984
+		// 1 << 1 is for runtime 1.10.980 and 1.10.984
 		constexpr void IsLayoutDependentNG(const bool a_value) noexcept { SetOrClearBit(structureIndependence, 1 << 1, a_value); }
 		// 1 << 2 is for runtime 1.11.137 and later
 		constexpr void IsLayoutDependentAE(const bool a_value) noexcept { SetOrClearBit(structureIndependence, 1 << 2, a_value); }
