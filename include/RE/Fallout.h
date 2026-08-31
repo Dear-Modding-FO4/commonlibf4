a646791cbbb88832265938391cd11669b25c3e9c
Kuz <kuzeygokk@outlook.com>
2026-08-30 17:31:33 -0700

    feat(rendering): define BSRenderPass layout

--- include/RE/Fallout.h
+++ include/RE/Fallout.h
@@ -410,6 +410,7 @@
 #include "RE/B/BSPrecomputedNavmeshInfoPathMap.h"
 #include "RE/B/BSRandom.h"
 #include "RE/B/BSReloadShaderI.h"
+#include "RE/B/BSRenderPass.h"
 #include "RE/B/BSResource.h"
 #include "RE/B/BSResourceNiBinaryStream.h"
 #include "RE/B/BSResourceStreamParser.h"
